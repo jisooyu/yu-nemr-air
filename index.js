36f0e155d9e4def05abe73a9c740eb218639a4ef
jisooyu <jisooyu@hotmail.com>
2021-09-19 20:18:06 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res)=> {
+    res.send({hi:'How are you doing?'})
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT)
