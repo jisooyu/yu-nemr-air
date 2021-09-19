3ece83f4705bdd3f9dcfa5a9d7a84b59fa82bf7e
jisooyu <jisooyu@hotmail.com>
2021-09-19 20:31:33 +0900

    trivial change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=> {
-    res.send({hi:'How are you doing?'})
+    res.send({hi:'Greate day!'})
 })
 
 const PORT = process.env.PORT || 5000
