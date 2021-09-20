fbf567ae8836df7ecb1556ab04c6e22211977893
jisooyu <jisooyu@hotmail.com>
2021-09-20 20:52:04 +0900

    adding google authentication

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+require('./services/passport')
+// const keys = require('./config/keys')
 
 const app = express()
 
-app.get('/', (req, res)=> {
-    res.send({hi:'Greate day!'})
-})
-
+// app.get('/', (req, res)=> {
+//     res.send({hi: 'what is a youth?'})
+// })
+require('./routes/authRoutes')(app)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
